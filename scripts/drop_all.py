68c3242eb4f242e910b47aed0958b12857485eef
Alan Du <alanhdu@gmail.com>
2017-10-08 15:18:11 -0400

    Fix drop_all.py

--- scripts/drop_all.py
+++ scripts/drop_all.py
@@ -1,4 +1,10 @@
+import sqlalchemy
+
 from nycodex import db
 
 if __name__ == "__main__":
-    db.Base.metadata.drop_all(db.engine)
+    metadata = sqlalchemy.MetaData()
+    metadata.reflect(bind=db.engine)
+    with db.engine.connect() as conn:
+        for key in metadata.tables:
+            conn.execute(f"DROP TABLE \"{key}\" CASCADE")

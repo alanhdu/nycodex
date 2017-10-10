1c5099457add8e77c5e87b64a1bbc1cd6e2188cc
Alan Du <alanhdu@gmail.com>
2017-10-09 23:54:18 -0400

    Fix drop_all.py script
    Drop types too

--- scripts/drop_all.py
+++ scripts/drop_all.py
@@ -3,6 +3,10 @@ import sqlalchemy
 from nycodex import db
 
 if __name__ == "__main__":
+    # Drop SQLAchemy Tables andTypes
+    db.Base.metadata.drop_all(db.engine)
+
+    # Drop scraped tables
     metadata = sqlalchemy.MetaData()
     metadata.reflect(bind=db.engine)
     with db.engine.connect() as conn:

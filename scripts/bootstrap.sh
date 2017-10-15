108c41a4a70f25011ad42b3db3a59f19bd98dcd2
Alan Du <alanhdu@gmail.com>
2017-10-14 20:17:11 -0400

    Install geopandas and geoalchemy2

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,3 +5,4 @@
 sudo -u postgres createuser --superuser adi
 sudo -u postgres createdb -E UTF8 -l en_US.UTF8 -T template0 -O adi nycodex
 sudo -u postgres echo "ALTER ROLE adi WITH PASSWORD 'password'" | sudo -u postgres psql -d nycodex
+sudo -u postgres echo "CREATE EXTENSION postgis;" | sudo -u postgres psql -d nycodex
